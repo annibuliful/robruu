ef374cfb687ee2f72505f8a147b5845bfe89a818
jarupong pajakgo <lagmanmc@gmail.com>
2016-10-14 16:37:51 +0700

    comment rating and search tested

--- backend/DB/feature/co_module.php
+++ backend/DB/feature/co_module.php
@@ -1,5 +1,7 @@
 <?php
-require 'C:\Users\Dell\Documents\GitHub\robruu\backend\DB\authen\authen_DB.php';
+
+declare(strict_types=1);
+require 'C:/Users/Dell/Documents/GitHub/robruu/backend/DB/authen/authen_DB.php';
 class Co_func
 {
     private $sql;
@@ -8,13 +10,13 @@ class Co_func
     private $follow_id;
     private $price;
     private $course_id;
-    public function __construct($user_id)
+    public function __construct(int $user_id)
     {
         $this->sql = new PDO('mysql:dbname=robruu_online;host=127.0.0.1', 'root', '@PeNtesterMYSQL');
         $this->sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->user_id = $user_id;
     }
-    public function new_feed($detail)
+    public function new_feed(string $detail)
     {
         $this->detail = $detail;
         $return = '';
@@ -34,8 +36,9 @@ class Co_func
 
         return (string) $return;
     }
-    public function following($follow_id)
+    public function following(int $follow_id)
     {
+        $return = '';
         $this->follow_id = $follow_id;
         $sql = $this->sql->prepare('SELECT * FROM user WHERE id = :user_id ;');
         $sql->bindParam(':user_id', $this->user_id);
@@ -47,9 +50,12 @@ class Co_func
             $sql->bindParam(':id_u', $this->user_id);
             $sql->bindParam(':id_f', $this->follow_id);
             $sql->execute();
+            $return = 'ติดตามแล้ว';
+
+            return (string) $return;
         }
     }
-    public function buy($course_id)
+    public function buy(int $course_id)
     {
         $this->course_id = $course_id;
         $sql = $this->sql->prepare('SELECT money FROM user WHERE id = :user_id');
@@ -70,14 +76,14 @@ class Co_func
                     $sql->execute();
                     $sql = $this->sql->prepare('UPDATE user SET
                                                 money = money - :price WHERE id = :id_user ; ');
-                    $sql->bindParam(':price',$fetch1['price'],PDO::PARAM_INT);
-                    $sql->bindParam(':id_user',$id_user,PDO::PARAM_INT);
+                    $sql->bindParam(':price', $fetch1['price'], PDO::PARAM_INT);
+                    $sql->bindParam(':id_user', $id_user, PDO::PARAM_INT);
                     $sql->execute();
                 } else {
                     echo 'เงินหรือคะแนนของคุณไม่พอ';
                 }
-            }else {
-              echo "ไม่มีคอร์สเรียนนี้";
+            } else {
+                echo 'ไม่มีคอร์สเรียนนี้';
             }
         } else {
             echo 'คุณยังไม่ได้เข้าสู่ระบบ';
@@ -85,4 +91,93 @@ class Co_func
             header('refresh: 3; url=index.php');
         }
     }
-}?>
+    public function comment(int $id_user, string $comment, int $id_post)
+    {
+        $id_users = $id_user;
+        $comments = $comment;
+        $id_posts = $id_post;
+        $sql = $this->sql->prepare('SELECT id_post,COUNT(id_post) FROM comment WHERE id_post = :id_post ;');
+        $sql->bindParam(':id_post', $id_post, PDO::PARAM_INT);
+        $sql->execute();
+        $fetch = $sql->fetch(PDO::FETCH_ASSOC);
+        if ($fetch) {
+            if ($comments != null) {
+                $sql = $this->sql->prepare('INSERT INTO comment(id_post,id_N,comment,time,id_user)
+                                        VALUES (:id_post ,:id_N ,:comment ,:time , :id_user)');
+                $this->sql->bindParam(':id_post', $id_post, PDO::PARAM_INT);
+                $this->sql->bindParam(':id_N', $fetch['COUNT(id_post)'], PDO::PARAM_INT);
+                $this->sql->bindParam(':comment', $comment, PDO::PARAM_STR);
+                $this->sql->bindParam(':time', date('D / m / Y'), PDO::PARAM_STR);
+                $this->sql->bindParam(':id_user', $id_user, PDO::PARAM_INT);
+                $this->sql->execute();
+            }
+        }
+    }
+    public function rating(int $id_user, int $id_question, int $id_video, int $id_playlist)
+    {
+        if ($id_question != null) {
+            $sql = $this->sql->prepare('SELECT id FROM question WHERE id = :id ; ');
+            $this->sql->bindParam(':id', $id_question, PDO::PARAM_INT);
+            $this->execute();
+            $fetch = $this->sql->fetch(PDO::FETCH_ASSOC);
+            if ($fetch) {
+                $sql = $this->sql->prepare('INSERT INTO rating(id_user,id_rating,type)
+                                        VALUES (:id_user ,:id_rating ,1);');
+                $sql->bindParam(':id_user', $id_user, PDO::PARAM_INT);
+                $sql->bindParam(':id_rating', $id_question, PDO::PARAM_INT);
+                $sql->execute();
+            }
+        } elseif ($id_playlist != null) {
+            $sql = $this->sql->prepare('SELECT id FROM playlist WHERE id = :id ;');
+            $this->sql->bindParam(':id', $id_playlist, PDO::PARAM_INT);
+            $this->execute();
+            $fetch = $this->sql->fetch(PDO::FETCH_ASSOC);
+            if ($fetch) {
+                $sql = $this->sql->prepare('INSERT INTO rating(id_user,id_rating,type)
+                                        VALUES (:id_user ,:id_rating ,2);');
+                $sql->bindParam(':id_user', $id_user, PDO::PARAM_INT);
+                $sql->bindParam(':id_rating', $id_playlist, PDO::PARAM_INT);
+                $sql->execute();
+            }
+        }
+    }
+    public function search(int $type, string $detail)
+    {
+        $return = array();
+        $detail = '%'.$detail.'%';
+        if ($type == 1) {
+            print_r($detail);
+            $course = $this->sql->prepare('SELECT course_name,price,id_author
+                                           FROM video_playlist WHERE course_name
+                                           LIKE :detail ; ');
+            $question = $this->sql->prepare('SELECT name,num_question FROM question_playlist
+                                             WHERE name LIKE :detail ; ');
+            $question->bindParam(':detail', $detail, PDO::PARAM_STR);
+            $question->execute();
+            $question_f = $question->fetchAll();
+            $course->bindParam(':detail', $detail, PDO::PARAM_STR);
+            $course->execute();
+            $course_f = $course->fetchAll(PDO::FETCH_ASSOC);
+            $return = array_merge($course_f, $question_f);
+
+            return (array) $return;
+        } elseif ($type == 2) {
+            $intructor = $this->sql->prepare('SELECT id_author
+                                         FROM video_playlist WHERE course_name
+                                         LIKE :detail ; ');
+            $intructor->bindParam(':detail', $detail, PDO::PARAM_STR);
+            $intructor->execute();
+
+            return $intructor->fetchAll(PDO::FETCH_ASSOC);
+        } elseif ($type == 3) {
+            $course = $this->sql->prepare('SELECT id_author
+                                       FROM video_playlist WHERE course_name
+                                       LIKE :detail ; ');
+            $course->bindParam(':detail', $detail, PDO::PARAM_STR);
+            $course->execute();
+
+            return $course->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+}
+?>
