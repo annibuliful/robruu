38043e522880e1a48d434d2cf0abe4ca00c63477
jarupong pajakgo <lagmanmc@gmail.com>
2016-10-17 19:32:02 +0700

    tested some function

--- backend/util/controller/intructor_controller.php
+++ backend/util/controller/intructor_controller.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 require 'C:/Users/Dell/Documents/GitHub/robruu/backend/DB/feature/intructor_module.php';
 class intructor_controller
 {
-  private $sql;
-  private $intruction;
+  private $intructor;
     public function __construct()
     {
-        $this->sql = new PDO('mysql:dbname=robruu_online;host=127.0.0.1', 'root', '@PeNtesterMYSQL');
-        $this->sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->intructor = new intructor();
     }
     public function question(array $picture,int $id_author,int $id_answer,array $choices, int $id_question,int $check_c,int $score)
@@ -22,8 +19,13 @@ class intructor_controller
           echo "false";
         }
     }
+    public function video(int $id_user, array $video)
+    {
+      $this->intructor->video_upload($id_user,$video);
+    }
+    public function make_course(int $id_user,int $id_video,string $course_name,int $price)
+    {
+      $this->intructor->make_course($id_user,$id_video,$course_name,$price);
+    }
 }
-$s = new intructor_controller();
-$s->question($_FILES['upload'],1,2,$_POST['email'],1,2,1);
-
 ?>
