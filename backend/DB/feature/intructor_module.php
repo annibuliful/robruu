33a83fb0fa7255362c84b96998f2b42643de6842
jarupong pajakgo <lagmanmc@gmail.com>
2016-10-17 19:20:33 +0700

    video_upload , make_course and update_course tested with controller function

--- backend/DB/feature/intructor_module.php
+++ backend/DB/feature/intructor_module.php
@@ -48,14 +48,15 @@ class intructor
             return false;
         }
     }
-    public function make_choices(array $choices, int $id_author, int $id_question, int $check_c)
+    public function make_choices(array $choices, int $id_author, int $id_question)
     {
         $sql = $this->pdo->prepare('SELECT id FROM picture WHERE id_author = :id ;');
         $sql->bindParam(':id', $id_question, PDO::PARAM_INT);
         $sql->execute();
         $fetch = $sql->fetch(PDO::FETCH_ASSOC);
         if ($fetch) {
-            $sql = $this->pdo->prepare('SELECT id_question FROM choice_question WHERE id_question = :id_question ;');
+            $sql = $this->pdo->prepare('SELECT id_question FROM choice_question
+                                        WHERE id_question = :id_question ;');
             $sql->bindParam(':id_question', $id_question, PDO::PARAM_INT);
             $sql->execute();
             $fetch = $sql->fetch(PDO::FETCH_ASSOC);
@@ -81,75 +82,80 @@ class intructor
         return false;
     }
 
-    public function video_upload($id_user, array $video, $dir, $price)
+    public function video_upload(int $id_user, array $video)
     {
-        $this->price = $price;
-        $this->dir = $dir;
-        $this->video = $video;
-        $this->id_user = $id_user;
-        $sql = $this->pdo->prepare('SELECT * FROM user WHERE username= :username ;');
-        $sql->bindParam(':username', $this->id_user);
+        $sql = $this->pdo->prepare('SELECT id FROM user WHERE id= :id_user ;');
+        $sql->bindParam(':id_user', $id_user);
         $sql->execute();
         $fetch = $sql->fetch(PDO::FETCH_ASSOC);
         if ($fetch) {
-            $sql = $this->pdo->prepare('INSERT INTO video(name,id_author,date,price)
-                                        VALUES (:video , :id_user , :date ,:price);');
-            $sql->bindParam(':video', $this->video['name'].$this->user_id, PDO::PARAM_STR);
-            $sql->bindParam(':id_user', $this->user_id, PDO::PARAM_INT);
-            $sql->bindParam('date', date('d / m / Y'));
-            $sql->bindParam(':price', $this->price, PDO::PARAM_INT);
+            $sql = $this->pdo->prepare('SELECT id FROM video WHERE name = :name ;');
+            $sql->bindParam(':name', $video['name'], PDO::PARAM_STR);
             $sql->execute();
-            move_uploaded_file($this->video['tmp_name'],
+            $fetch = $sql->fetch(PDO::FETCH_ASSOC);
+            if (!$fetch) {
+                $sql = $this->pdo->prepare('INSERT INTO video(name,id_author,date,price) VALUES (:video , :id_user , :date ,1);');
+                $sql->bindParam(':video', $video['name'], PDO::PARAM_STR);
+                $sql->bindParam(':id_user', $id_user, PDO::PARAM_INT);
+                $sql->bindParam('date', date('d / m / Y'));
+            //$sql->bindParam(':price', $price, PDO::PARAM_INT);
+            $sql->execute();
+                move_uploaded_file($video['tmp_name'],
                                'C:/Users/Dell/Documents/GitHub/robruu/backend/store/videos/'.$video['name']);
+
+                return true;
+            } else {
+                return false;
+            }
         } else {
             echo 'ไม่มีชื่อผู้ใช้งาน';
             echo 'กำลังพาไปหน้าหลัก....';
             header('refresh: 3; url=index.php');
+
+            return false;
         }
+
+        return false;
     }
-    public function make_course($id_user, $id_video, $course_name, $price)
+    public function make_course(int $id_user, int $id_video, string $course_name, int $price)
     {
-        $this->id_user = $id_user;
-        $this->id_video = $id_video;
-        $this->course_name = $course_name;
-        $this->price = $price;
-        $sql = $this->pdo->prepare('SELECT id FROM user WHERE id = :id_user');
-        $sql->bindParam(':id_user', $this->id, PDO::PARAM_INT);
-        $Sql->execute();
+        $sql = $this->pdo->prepare('SELECT COUNT(id_video),id_video,id_playlist FROM video_playlist WHERE
+                                    course_name = :course_name AND id_video = :id_video
+                                    AND id_author = :id_author ;');
+        $sql->bindParam(':course_name',$course_name,PDO::PARAM_STR);
+        $sql->bindParam(':id_video',$id_video,PDO::PARAM_INT);
+        $sql->bindParam(':id_author',$id_user,PDO::PARAM_INT);
+        $sql->execute();
         $fetch = $sql->fetch(PDO::FETCH_ASSOC);
-        if ($fetch) {
-            $sql = $this->pdo->prepare('SELECT COUNT(id_video) FROM video_playlist WHERE
-                                    course_name = :course_name AND id_user = :id_author ;');
-            $sql->bindParam(':course_name', $this->course_name);
-            $sql->bindParam('id_author', $this->id_user);
-            $sql->execute();
-            $fetch = $sql->fetch(PDO::FETCH_COLUMN);
-            if (!$fetch) {
-                $sql = $this->pdo->prepare('INSERT INTO video_playlist(course_name,price,id_video,id_user,flag_num)
-                                      VALUES (:course_name ,0,:id_video ,:id_user ,1);');
-                $sql->bindParam(':course_name', $this->course_name);
-                $sql->bindParam(':id_video', $this->video_id);
-                $sql->bindParam(':id_user', $this->id_user);
-                $sql->execute();
-                echo 'สร้าง playlist เรียบร้อย';
-            } else {
-                update_course($id_video, $id_user, $course_name, $fetch[0]);
-            }
-        } else {
-            echo 'คุณยังไม่ได้เข้าสู่ระบบ';
-            echo 'กำลังพาไปหน้าหลัก....';
-            header('refresh: 3; url=index.php');
+        $flag = $fetch['COUNT(id_video)'] + 1 ;
+        print_r($fetch);
+        if ($fetch['id_video'] != $id_video) {
+          $sql = $this->pdo->prepare('INSERT INTO video_playlist(id_playlist,course_name,price,
+                                      id_video,id_author,flag_num) VALUES (:id_playlist ,:course_name ,:price,
+                                      :id_video,:id_author,:flag_num ) ;');
+          $sql->bindParam(':id_playlist',uniqid(),PDO::PARAM_STR);
+          $sql->bindParam(':course_name',$course_name,PDO::PARAM_STR);
+          $sql->bindParam(':price',$price,PDO::PARAM_INT);
+          $sql->bindParam(':id_video',$id_video,PDO::PARAM_INT);
+          $sql->bindParam(':id_author',$id_user,PDO::PARAM_INT);
+          $sql->bindParam(':flag_num',$flag,PDO::PARAM_INT);
+          $sql->execute();
+          echo "inserted";
+          return true;
+        }else {
+          $this->update_course($id_video,$id_user,$course_name,$flag,$fetch['id_playlist']);
+          echo "updated";
         }
     }
-    public function update_course(int $id_video,int $id_user,int $id_course,int $flag_num)
+    public function update_course(int $id_video, int $id_user, string $course_name, int $flag_num,string $id_playlist)
     {
-        $sql = $this->pdo->prepare('INSERT INTO video_playlist(course_name,price,id_video,id_user,flag_num)
-                                VALUES (:course_name ,0,:id_video ,:id_user ,:flag_num)');
+        $sql = $this->pdo->prepare('INSERT INTO video_playlist(id_playlist,course_name,price,id_video,id_author,flag_num)
+                                VALUES (:id_playlist,:course_name ,0,:id_video ,:id_author ,:flag_num ) ;');
+        $sql->bindParam(':id_playlist',$id_playlist,PDO::PARAM_STR);
         $sql->bindParam(':course_name', $course_name, PDO::PARAM_STR, 120);
         $sql->bindParam(':id_video', $id_video, PDO::PARAM_INT, 50);
-        $sql->bindParam(':id_user', $id_user, PDO::PARAM_INT, 10);
-        $sql->bindParam(':flag_num', $flag_num, PDO::PARAM_INT, 4);
+        $sql->bindParam(':id_author', $id_user, PDO::PARAM_INT, 10);
+        $sql->bindParam(':flag_num', $flag_num, PDO::PARAM_INT);
         $sql->execute();
     }
 }
-?>
