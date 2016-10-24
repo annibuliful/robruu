fb3f6caffaaf7c0d168783ecd5ac656437754480
jarupong pajakgo <lagmanmc@gmail.com>
2016-10-25 02:46:21 +0700

    answer and list course function tested withe view

--- backend/util/controller/student_controller.php
+++ backend/util/controller/student_controller.php
@@ -1,19 +1,39 @@
 <?php
 declare(strict_types=1);
 require 'C:/Users/Dell/Documents/GitHub/robruu/backend/DB/feature/student_module.php';
+require 'C:/Users/Dell/Documents/GitHub/robruu/backend/util/view/student_view.php';
 class student_controller
 {
-  private $sql;
   private $student;
+  private $view ;
     public function __construct()
     {
         $this->student = new student();
+        $this->view = new student_view();
     }
     public function answer(int $id_question,int $id_answer,int $id_user)
     {
-      $this->student->answer($id_question,$id_answer,$id_user);
+       $check = $this->student->answer($id_question,$id_answer,$id_user);
+      if ($check == 'true') {
+        echo "1";
+        $this->view->answer_true();
+      }elseif ($check == 'false') {
+        echo "2";
+        $this->view->answer_false();
+      }elseif ($check == 'answered') {
+        echo "3";
+        $this->view->answer_answered();
+      }
+
+    }
+    public function list_course(int $id_user)
+    {
+        $check = $this->student->list_course($id_user);
+        if ($check != null) {
+          $this->view->list_course($check);
+        }
     }
 }
-
-
+// tested with answer function
+// tested with list course function 
  ?>
