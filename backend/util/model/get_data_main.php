3982106770ecc3d95197a6d3b09945e79765c472
jarupong pajakgo <lagmanmc@gmail.com>
2016-10-25 02:55:40 +0700

    model for main page

--- backend/util/model/get_data_main.php
+++ backend/util/model/get_data_main.php
@@ -8,23 +8,41 @@ class get_data_main
     public function __construct()
     {
         $this->sql = new PDO('mysql:host=localhost;dbname=robruu_online', 'root', '@PeNtesterMYSQL');
+        $this->sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     public function question(int $flag = 1)
     {
         $flag = $flag - 1;
-        $sql = $this->sql->prepare('SELECT * FROM question LIMIT :flag ,15 ;');
-        $sql->bindParam(':flag', $this->flag, PDO::PARAM_INT);
-        $sql->execute();
-
-        return (array) $sql->fetchAll(PDO::FETCH_ASSOC);
+        if ($flag == null) {
+          $sql = $this->sql->prepare('SELECT * FROM question LIMIT 0 ,15 ;');
+          $sql->execute();
+          return (array) $sql->fetchAll(PDO::FETCH_ASSOC);
+        }else {
+          $sql = $this->sql->prepare('SELECT * FROM question LIMIT :flag ,15 ;');
+          $sql->bindParam(':flag',$flag,PDO::PARAM_INT);
+          $sql->execute();
+          return (array) $sql->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
-    public function courses()
+    public function courses(int $flag = 1)
     {
       $flag = $flag - 1;
-      $sql = $this->sql->prepare('SELECT * FROM video_playlist WHERE flag_num = 1 LIMIT :flag ,15 ;');
-      $sql->bindParam(':flag', $this->flag, PDO::PARAM_INT);
+      if ($flag == null) {
+          $sql = $this->sql->prepare('SELECT * FROM video_playlist WHERE flag_num = 1 LIMIT 0 ,15 ;');
+          $sql->execute();
+        return (array) $sql->fetchALL(PDO::FETCH_ASSOC);
+      }else {
+        $sql = $this->sql->prepare('SELECT * FROM video_playlist WHERE flag_num = 1 LIMIT :flag ,15 ;');
+        $sql->bindParam(':flag', $this->flag, PDO::PARAM_INT);
+        $sql->execute();
+      return (array) $sql->fetchALL(PDO::FETCH_ASSOC);
+      }
+    }
+    public function list_choice(int $id_question){
+      $sql = $this->sql->prepare('SELECT * FROM choice_question ');
+      $sql->bindParam(':id_question',$id_question,PDO::PARAM_INT);
       $sql->execute();
-
       return (array) $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 }
+?>
