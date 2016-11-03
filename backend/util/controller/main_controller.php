6e98f9a9a8ebe32920d45ed65b4dad89f4719642
jarupong pajakgo <lagmanmc@gmail.com>
2016-11-03 20:17:53 +0700

    user controller

--- backend/util/controller/main_controller.php
+++ backend/util/controller/main_controller.php
@@ -1,15 +1,23 @@
 <?php
+declare(strict_types=1);
+require 'C:/Users/Dell/Documents/GitHub/robruu/backend/DB/feature/user.php';
+require 'C:/Users/Dell/Documents/GitHub/robruu/backend/util/view/authen_view.php';
 
 class main_controller
 {
-
+  private $user;
+  private $view;
   function __construct()
   {
+    $this->user = new user();
+    $this->view = new authen_view();
   }
-  public function question(int $flag)
+  public function user(int $user)
   {
-
+    $check = $this->user->user_detail($user);
+    if ($check != null) {
+      $this->view->loged($check);
+    }
   }
 }
-
  ?>
